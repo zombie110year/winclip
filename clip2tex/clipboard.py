dd5dfd2f7dde82e1d97951ccb38279c22bc3617d
zombie110year <zombie110year@outlook.com>
2019-10-27 14:24:50 +0800

    :pencil: update documentation.

--- clip2tex/clipboard.py
+++ clip2tex/clipboard.py
@@ -1,15 +1,57 @@
 """剪贴板
+======
 
-监听剪贴板变动
-读写剪贴板
+1. 监听剪贴板变动
+2. 读写剪贴板
+
+Clipboard
+=========
+
+1. watch clipboard updating
+2. read/write clipboard
 """
 import win32.win32clipboard as wc
 from time import sleep
 
+__all__ = ("ClipBoard")
+
 
 class ClipBoard:
+    """Windows ClipBoard
+
+    Support ``read/write`` and ``listen``.
+
+    Example
+    =======
+
+    You can specify which format to read
+
+    >>> ClipBoard.read(win32.win32clipboard.CF_UNICODETEXT)
+    "Hello World"
+
+    or auto detect the first format of ``EnumClipboardFormats()``
+    If you copied a text, the default format is ``CF_OEMTEXT``
+    (``bytes`` in python)
+
+    >>> ClipBoard.read()
+    b"Hello World"
+
+    If You want to copy a image, should be a BitMap, and read/write it as a ``CF_DIB``
+    (Device Independent Bitmap)
+
+    >>> ClipBoard.write(b"BM...")
+
+    You can listen clipboard change event by ``listen(lambda : ...)``, default interval is 0.1 s.
+
+    >>> ClipBoard.listen(lambda : print("clip update"))
+    """
     @staticmethod
     def listen(callback, interval=0.1):
+        """Watch Clipboard Update, if updated, run callback without arguments.
+
+        :param callback: a function without parameters.
+        :param float interval: interval to check update.
+        """
         clip_seq = wc.GetClipboardSequenceNumber()
         while True:
             clip_seq_new = wc.GetClipboardSequenceNumber()
@@ -21,6 +63,11 @@ class ClipBoard:
 
     @staticmethod
     def read(format=None) -> bytes:
+        """read data from clipboard
+
+        :param int format: the clipboard format from winuser.h, ``CF_*`` constants.
+                           if ``None``, will use first format which ``EnumClipboardFormats`` return.
+        """
         if format is None:
             format = enum_clipformats()[0]
         try:
@@ -31,7 +78,12 @@ class ClipBoard:
         return data
 
     @staticmethod
-    def write(data: bytes, format=wc.CF_UNICODETEXT):
+    def write(data, format=wc.CF_UNICODETEXT):
+        """write contents to clipboard.
+
+        :param data: str or bytes
+        :param int format: constants defined in winuser.h, the clipboard format.
+        """
         try:
             wc.OpenClipboard()
             wc.EmptyClipboard()
