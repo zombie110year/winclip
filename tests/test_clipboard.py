81d4f60df580aabdf77cdec82a5ce8faab166ea8
zombie110year <zombie110year@outlook.com>
2019-10-27 13:47:57 +0800

    添加单元测试

--- tests/test_clipboard.py
+++ tests/test_clipboard.py
@@ -6,5 +6,18 @@ from clip2tex.clipboard import ClipBoard
 def test_rw_text():
     text = "Hello World"
     ClipBoard.write(text)
-    text_r = ClipBoard.read()
+    text_r = ClipBoard.read(wc.CF_UNICODETEXT)
     assert text == text_r
+    text_rb = ClipBoard.read(wc.CF_OEMTEXT)
+    assert text_rb == text.encode("ascii")
+
+
+def test_rw_bitmap():
+    with open("tests/msdn-logo.bmp", "rb") as bmp:
+        data = bmp.read()
+    ClipBoard.write(data, wc.CF_DIB)
+
+    data1 = ClipBoard.read(wc.CF_DIB)
+
+    # data1 尾部会多一个 0x00
+    assert data1.startswith(data)
